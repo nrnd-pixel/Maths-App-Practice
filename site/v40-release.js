fcbbec6bee3a649b4d62d7b3560ef7079ff1c0c6
nrnd-pixel <anuarn.edu@gmail.com>
2026-08-27 14:15:56 +0800

    Preserve V46 loader key

--- site/v40-release.js
+++ site/v40-release.js
@@ -47,7 +47,7 @@
     loadScriptOnce('v44-intervention-highlight-clarity.js?v=44c-clarity-1', 'data-v44c-intervention-highlight-clarity');
     loadScriptOnce('v45-intervention-queue.js?v=45a-1', 'data-v45a-intervention-queue');
     loadScriptOnce('v45-intervention-outcomes.js?v=45b-1', 'data-v45b-intervention-outcomes');
-    loadScriptOnce('v46-intervention-export.js?v=46a-1', 'data-v46-intervention-export');
+    loadScriptOnce('v46-intervention-export.js?v=46a-1', 'data-v46a-intervention-export');
     loadScriptOnce('v47-intervention-history.js?v=47a-1', 'data-v47a-intervention-history');
     loadScriptOnce('v47-follow-up-from-history.js?v=47b-1', 'data-v47b-follow-up-from-history');
     loadScriptOnce('v47-class-intervention-overview.js?v=47c-1', 'data-v47c-class-intervention-overview');
