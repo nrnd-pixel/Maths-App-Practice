72c95e3e3b0a28f9906903ac71895befb62203fb
nrnd-pixel <anuarn.edu@gmail.com>
2026-09-01 08:36:47 +0800

    Restore final production loader identity for V5.3D5

--- site/v40-release.js
+++ site/v40-release.js
@@ -47,7 +47,7 @@
     loadScriptOnce('v45-intervention-queue.js?v=45a-1', 'data-v45a-intervention-queue');
     loadScriptOnce('v45-intervention-outcomes.js?v=45b-1', 'data-v45b-intervention-outcomes');
     loadScriptOnce('v46-intervention-export.js?v=46a-1', 'data-v46a-intervention-export');
-    loadScriptOnce('v47-intervention-history.js?v=47a-1', 'data-v47-intervention-history');
+    loadScriptOnce('v47-intervention-history.js?v=47a-1', 'data-v47a-intervention-history');
     loadScriptOnce('v47-follow-up-from-history.js?v=47b-1', 'data-v47b-follow-up-from-history');
     loadScriptOnce('v47-class-intervention-overview.js?v=47c-1', 'data-v47c-class-intervention-overview');
     loadScriptOnce('v48-teacher-deadline-monitoring.js?v=48a-2', 'data-v48a-teacher-deadline-monitoring');
