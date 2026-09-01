3a6d21af544a0056f096ea417a5602d9df93540f
nrnd-pixel <anuarn.edu@gmail.com>
2026-09-01 18:11:03 +0800

    V5.4F wire bulk selection scope safety

--- site/v40-release.js
+++ site/v40-release.js
@@ -110,6 +110,7 @@
     loadScriptOnce('v54c-compact-question-bank.js?v=54c-1', 'data-v54c-compact-question-bank');
     loadScriptOnce('v54d-topical-resource-simplification.js?v=54d-1', 'data-v54d-topical-resource-simplification');
     loadScriptOnce('v54e-bulk-practice-eligibility.js?v=54e-1', 'data-v54e-bulk-practice-eligibility');
+    loadScriptOnce('v54f-bulk-selection-scope-safety.js?v=54f-1', 'data-v54f-bulk-selection-scope-safety');
   }
 
   if (document.readyState === 'loading'){
