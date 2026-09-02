95473ea70af63f29f7e8aa81da39075935145ffc
nrnd-pixel <anuarn.edu@gmail.com>
2026-09-02 22:30:09 +0800

    Platform: remove automatic Science-only redirect race

--- site/config.js
+++ site/config.js
@@ -87,8 +87,7 @@ window.addEventListener('load', () => {
     './platform-signin-owner-v01.js',
     './platform-subject-access-v01.js',
     './platform-year-launch-v01.js',
-    './platform-subject-home-v01.js',
-    './platform-science-only-redirect-v01.js'
+    './platform-subject-home-v01.js'
   ].forEach(src => {
     const script = document.createElement('script');
     script.src = src;
