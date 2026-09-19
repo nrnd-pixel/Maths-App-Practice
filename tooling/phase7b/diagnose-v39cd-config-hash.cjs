8c8829f71033289c59622afbbccff604f0a46836
nrnd-pixel <anuarn.edu@gmail.com>
2026-09-19 09:38:52 +0800

    Phase 7B-L: remove temporary V39CD diagnostic/shadow builder

--- tooling/phase7b/diagnose-v39cd-config-hash.cjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require('node:fs');
-const path = require('node:path');
-const { createHash } = require('node:crypto');
-
-const root = path.resolve(__dirname, '../..');
-const source = fs.readFileSync(path.join(root, 'site/config.js'), 'utf8').replace(/\r\n/g, '\n');
-console.log('PHASE7BL_CONFIG_SHA256 ' + createHash('sha256').update(source).digest('hex'));
